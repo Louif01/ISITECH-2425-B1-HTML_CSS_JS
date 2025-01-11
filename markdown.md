10990a27001d05f49f9701af2d945039b10099ca
Louis Faure <dadou.faure@outlook.fr>
2025-01-11 10:40:01 +0100

    Premier commit

---
+++ markdown.md
@@ -0,0 +1,33 @@
+# Commande pour initailiser le projet sur github
+
+## Terminal VSCODE
+
+<!-- Initialiser le dépôt -->
+
+```bash
+git init
+```
+
+<!-- Ajouter des fichiers -->
+
+```bash
+git add .
+```
+<!-- Faire un premier commit -->
+
+```bash
+git commit -m "Premier commit"
+```
+<!-- Ajouter le dépôt distant -->
+
+```bash
+git remote add origin [URL_DU_DEPOT] 
+```
+Bien pensé à mettre le jeton, et peut-être en créer un autre, à voir....
+
+<!-- Pousser les changements -->
+
+```bash
+git push -u origin master
+```
+
---
+++ script.js
@@ -0,0 +1,71 @@
+document.getElementById('image_7').addEventListener('click', function() { 
+    this.classList.add('rotate'); 
+    
+    // Retirer la classe après l'animation pour pouvoir la rejouer 
+    setTimeout(() => { 
+        this.classList.remove('rotate');
+    }, 1000); // La durée doit correspondre à celle de l'animation CSS
+});
+
+document.getElementById('image_2').addEventListener('click', function() { 
+    this.classList.add('rotate'); 
+    
+    // Retirer la classe après l'animation pour pouvoir la rejouer 
+    setTimeout(() => { 
+        this.classList.remove('rotate');
+    }, 1000); // La durée doit correspondre à celle de l'animation CSS
+});
+
+document.getElementById('image_3').addEventListener('click', function() { 
+    this.classList.add('rotate'); 
+    
+    // Retirer la classe après l'animation pour pouvoir la rejouer 
+    setTimeout(() => { 
+        this.classList.remove('rotate');
+    }, 1000); // La durée doit correspondre à celle de l'animation CSS
+});
+
+document.getElementById('image_10').addEventListener('click', function() { 
+    this.classList.add('rotate'); 
+    
+    // Retirer la classe après l'animation pour pouvoir la rejouer 
+    setTimeout(() => { 
+        this.classList.remove('rotate');
+    }, 1000); // La durée doit correspondre à celle de l'animation CSS
+});
+
+document.getElementById('image_5').addEventListener('click', function() { 
+    this.classList.add('rotate'); 
+    
+    // Retirer la classe après l'animation pour pouvoir la rejouer 
+    setTimeout(() => { 
+        this.classList.remove('rotate');
+    }, 1000); // La durée doit correspondre à celle de l'animation CSS
+});
+
+document.getElementById('image_6').addEventListener('click', function() { 
+    this.classList.add('rotate'); 
+    
+    // Retirer la classe après l'animation pour pouvoir la rejouer 
+    setTimeout(() => { 
+        this.classList.remove('rotate');
+    }, 1000); // La durée doit correspondre à celle de l'animation CSS
+});
+
+document.getElementById('image_8').addEventListener('click', function() { 
+    this.classList.add('rotate'); 
+    
+    // Retirer la classe après l'animation pour pouvoir la rejouer 
+    setTimeout(() => { 
+        this.classList.remove('rotate');
+    }, 1000); // La durée doit correspondre à celle de l'animation CSS
+});
+
+document.getElementById('image_9').addEventListener('click', function() { 
+    this.classList.add('rotate'); 
+    
+    // Retirer la classe après l'animation pour pouvoir la rejouer 
+    setTimeout(() => { 
+        this.classList.remove('rotate');
+    }, 1000); // La durée doit correspondre à celle de l'animation CSS
+});
